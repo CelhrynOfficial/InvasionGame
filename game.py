d040706a67f5e9e9bd0d3d733dcf3f6eeed4920c
Jace020 <evanbarbierveillon@gmail.com>
2024-03-26 14:17:26 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,13 +2,14 @@
 import pygame
 from pygame.locals import *
 import sys
+import time
 
 
 
 #Création des classes
 class Ship:
     def __init__(self,x,y):
-        self.sprit=pygame.image.load("player.png")
+        self.sprit=pygame.image.load("bob4.svg")
         self.x = x
         self.y = y
 
@@ -31,39 +32,59 @@ class App:
         """
             Initialisation de la fenêtre et des éléments
         """
-        # Initialise screen
-        pygame.init()
-        infoObject = pygame.display.Info()
-        screen=pygame.display.set_mode((infoObject.current_w, infoObject.current_h))
-        pygame.display.set_caption('Invaders')
-
-        # Fill background
-        background = pygame.Surface(screen.get_size())
-        background = background.convert()
-        background.fill((250, 250, 250))
-
-
-        # Blit everything to the screen
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
+        
 
         #Afficher le vaisseau
         screen.blit(background, (0, 0))
-        ship=Ship(infoObject.current_w/2,infoObject.current_h-(infoObject.current_w/20))
-        ship.draw()
+        self.ship=Ship((infoObject.current_w/2)-20,(infoObject.current_h-(infoObject.current_w/20))-20)
+
+
+    def update(self):
+
+        events = pygame.event.get()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.ship.move(-1,0)
+            if event.key == pygame.K_RIGHT:
+                self.ship.move(1,0)
+            if event.key == pygame.K_DOWN:
+                self.ship.move(0,1)
+            if event.key == pygame.K_UP:
+                self.ship.move(0,-1)
+
+    def draw(self):
+        # On affiche le vaisseau
+        screen.fill((255,255,255))
+        self.ship.draw()
 
 
 
+# Initialise screen
+pygame.init()
+infoObject = pygame.display.Info()
+screen=pygame.display.set_mode((infoObject.current_w-20, infoObject.current_h-20))
+pygame.display.set_caption('Invaders')
+
+# Fill background
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+background.fill((250, 250, 250))
 
 
+# Blit everything to the screen
+screen.blit(background, (0, 0))
+pygame.display.flip()    
+    
+appli=App()
 
+while True:
 
-
-
-while 1:
+    #time.sleep(5/10000)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    App()
+    
     
 
-    
+    appli.update()
+    appli.draw()
