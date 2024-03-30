4632905c7431f5855c1dbf644a6fd22a81b2ce55
Jace020 <evanbarbierveillon@gmail.com>
2024-03-30 12:31:03 +0100

    Update Game2.py

--- Game2.py
+++ Game2.py
@@ -18,7 +18,7 @@ class Ship:
     def __init__(self, x, y):
         self.sprite = pygame.image.load("player.png")
         self.rect= self.sprite.get_rect(x=x, y=y)
-        self.speed=5
+        self.speed=1
         self.velocity=[0,0]
         self.bullets=[]
 
@@ -55,6 +55,11 @@ class enemie:
     def __init__(self, x, y):
         self.sprite=pygame.image.load("player.png")
         self.rect=self.sprite.get_rect(x=x, y=y)
+        self.speed=1
+        self.velocity=[1,0]
+    
+    def move(self):
+        self.rect.move_ip(self.velocity[0]*self.speed, self.velocity[1]*self.speed )
 
     def draw(self):
         screen.blit(self.sprite, self.rect) #Je place mon missile à sa position
@@ -62,9 +67,9 @@ class enemie:
 class band:
     def __init__(self):
         self.band=[]
-        for i in range(11):
+        for i in range(8):
             for j in range(3):
-                enemies=enemie((i+0.6)*64, (j*100)+10)
+                enemies=enemie((i+0.3)*64*1.5, (j*100)+20)
                 self.band.append(enemies)
 
 class App:
@@ -74,10 +79,12 @@ class App:
             self.speed = speed
             self.ship.speed=self.speed
             self.pressed_keys = []  # Liste pour stocker les touches enfoncées
-            self.time=0 #Variables me permettant de gere l'envoie des laser
-            self.timer=0
+            self.timel=0 #Variables me permettant de gere l'envoie des laser
+            self.timerl=0
+            self.timem=0
+            self.timerm=0
             self.groupe=band()
-            self.ship.shot()
+            #self.ship.shot()
 
         
         
@@ -100,20 +107,32 @@ class App:
             self.ship.velocity[0]=0
         self.ship.move()
 
-        
+        #Faire tirer le vaisseau
         if pygame.K_SPACE in self.pressed_keys:
-            self.timer=time.time()
-            if self.timer-self.time>=0.7: #Cette conditionelle empche de tirer le missile trop vite
+            self.timerl=time.time()
+            if self.timerl-self.timel>=0.7: #Cette conditionelle empche de tirer le missile trop vite
                 self.ship.shot()
-                self.time=self.timer
+                self.timel=self.timerl
         
                 
         # Mettez à jour la position de tous les lasers
         for bullet in self.ship.bullets:
             bullet.velocity[1] = -1*self.speed
             bullet.move()
-        
 
+        
+        #Mettre à jour la position des enemies
+        for enemie in self.groupe.band:
+            
+            infoObject = pygame.display.Info()
+            w=infoObject.current_w
+            
+        
+            if enemie.rect.left < 0 or enemie.rect.right >w :
+                enemie.velocity[0] = -enemie.velocity[0]*enemie.speed
+             
+            enemie.move()
+        
         # Supprimez les lasers qui ont quitté l'écran
         self.ship.bullets = [bullet for bullet in self.ship.bullets if bullet.rect.y > -bullet.sprite.get_height()]
 
@@ -121,13 +140,8 @@ class App:
         infoObject = pygame.display.Info()
         self.ship.rect.y= (infoObject.current_h - (infoObject.current_h)/20)-65
 
-        #Verifier les colision
-        for bullets in self.ship.bullets:
-            for mob in self.groupe.band:
-                if  bullets.colliderect(mob)==1:
-                    print(1)
-                else:
-                    print(0)
+        #Verifier les colision entre les lasers et les enemies
+        
         
 
     def draw(self):
@@ -138,6 +152,8 @@ class App:
             
         for bullet in self.ship.bullets:  # Dessinez tous les lasers
             bullet.draw()
+
+
         pygame.display.flip()  # J'affiche tous les sprites
 
 
@@ -151,7 +167,7 @@ background = pygame.Surface(screen.get_size()) #Je crée mon fond d'écran
 background = background.convert()
 #background.fill((250, 250, 250)) #Je nettoie l'écran   
     
-appli=App(1) #Je définie mon application, avec une valeur de vitesse
+appli=App(2) #Je définie mon application, avec une valeur de vitesse
 
 
 key_events = []  # Liste pour stocker les événements clavier
