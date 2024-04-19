4f5dbdd915cc9b70cd113bd7b194cee5aab93e7e
Jace020 <evanbarbierveillon@gmail.com>
2024-04-19 15:08:11 +0200

    Update bob game.py

--- bob game.py
+++ bob game.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.locals import *
 import sys
-import time
+import time 
+import random
 
 # Définition des couleurs
 WHITE = (255, 255, 255)
@@ -51,50 +52,109 @@ class Bullet:
         screen.blit(self.sprite, self.rect)
 
     
-
-            
-
 class enemie:
-    def __init__(self, x, y):
-        self.sprite=pygame.image.load("bull.svg")
+    def __init__(self, x, y,speed=1):
+        self.sprite=pygame.image.load("jellyy.png")
         self.rect=self.sprite.get_rect(x=x, y=y)
-        self.speed=1
+        self.speed=speed
         self.velocity=[1,0]
-        
-    
+       
+
     def move(self):
         self.rect.move_ip(self.velocity[0]*self.speed, self.velocity[1]*self.speed )
 
     def draw(self):
         screen.blit(self.sprite, self.rect) #Je place mon missile à sa position
 
+    
+
+
+class enemie_b:
+    def __init__(self, x, y): #J'initialise mon 'Bullet' enemie
+        self.sprite=pygame.image.load("bull.svg") #Son sprite
+        self.rect= self.sprite.get_rect(x=x, y=y)
+        self.speed=1
+        self.velocity=[0,0]
+
+    def move(self):
+        self.rect.move_ip(self.velocity[0]*self.speed, self.velocity[1]*self.speed )
+
+    def draw(self):
+        """
+            Affichage du missile enemie
+        """
+        screen.blit(self.sprite, self.rect)
+
 class band:
     def __init__(self):
         self.band=[]
-        self.touch=0
+        self.bullets=[]
+        self.speed=1
         for i in range(8):
-            for j in range(3):
-                enemies=enemie((i+0.3)*64*1.5, (j*100)+20)
+            for j in range(3) :
+                enemies=enemie((i+0.3)*64*1.5, (j*100)+20, self.speed)
                 self.band.append(enemies)
+    
+    def add(self):
+        enemies=enemie((0.3)*64*1.5, 20, self.speed)
+        self.band.append(enemies)
 
+    def shot(self):
+         
+        i=random.randint(0, len(self.band)-1)
+        x=self.band[i].rect.x
+        y=self.band[i].rect.y
+        bullet_e = enemie_b(x, y)  # Créez un nouveau laser
+        self.bullets.append(bullet_e)  # Ajoutez le laser à la liste
+       
+        
+
+class score:
+    def __init__(self):
+        self.score=0
+        self.font=pygame.font.Font(None, 24)
+        
+    
+    def draw(self):
+        scr=str(self.score)
+        txt="Score: "+ scr
+        text = self.font.render(txt,1,(255,0,255))
+        screen.blit(text, (0,0))
+
+        
 class App:
     def __init__(self, speed=1):
             screen.blit(background, (0, 0))
             
+            self.game=True
+
             self.ship = Ship((infoObject.current_w / 2) - 20, (infoObject.current_h - (infoObject.current_h / 10)) - 20)
-            self.speed = speed
-            self.ship.speed=self.speed
+            self.ship.speed=speed*2.2 #Création du vaisseau
+
             self.pressed_keys = []  # Liste pour stocker les touches enfoncées
-            self.timel=0 #Variables me permettant de gere l'envoie des laser
+
+            self.timel=0 #Variables me permettant de gere l'envoie des laser du joueur
             self.timerl=0
-            self.anc=0
-            self.groupe=band()
+
+            self.groupe=band() #Je crée les enemies
+
+            self.tog=time.time() #Variable me permettant de gerer le changement de vitesse des enemies 
+            self.anctog=time.time()
+
+            self.cycle=0 #Variable qui mémorise le nombre de cycle du jeu
+
+            self.score=score() #Le score
+
         
 
         
         
 
     def update(self, key_events):
+
+        self.cycle+=1 #A chaque fois que je fais un cycle je le mémorise
+        #print(self.cycle)
+
         for event in key_events:
             if event.type == pygame.KEYDOWN:
                 if event.key not in self.pressed_keys:  # Ajoutez la touche à la liste si elle n'y est pas déjà
@@ -124,58 +184,112 @@ class App:
         #Faire tirer le vaisseau
         if pygame.K_SPACE in self.pressed_keys:
             self.timerl=time.time()
-            if self.timerl-self.timel>=0.5: #Cette conditionelle empche de tirer le missile trop vite
+            if self.timerl-self.timel>=0.5: #Cette conditionelle empeche de tirer le missile trop vite
                 self.ship.shot()
                 self.timel=self.timerl
         
                 
-        # Mettez à jour la position de tous les lasers
+       # Mettez à jour la position de tous les lasers
+        bullets_to_remove = []  # Liste temporaire pour stocker les balles à supprimer
         for bullet in self.ship.bullets:
-            bullet.velocity[1] = -1*self.speed
+            bullet.velocity[1] = -1 
             bullet.move()
 
-            #Verifier les colision entre les lasers et les enemies
+            # Vérifier les collisions entre les lasers et les ennemis
+            for enemy in self.groupe.band:
+                if bullet.rect.colliderect(enemy.rect):
+                    self.groupe.band.remove(enemy)
+                    bullets_to_remove.append(bullet)  # Ajoutez la balle à la liste temporaire
+                    self.score.score= self.score.score+100 #Si un ennemei est detruit je rajoue 100 points à mon score
+                    self.groupe.add()
+
+      
+                    
+                  
+
+        # Supprimez les balles de la liste originale
+        for bullet in bullets_to_remove:
+            if bullet in self.ship.bullets:  # Vérifiez si la balle est toujours dans la liste
+                self.ship.bullets.remove(bullet)
+
+        #Faire tirer les ennemis
+        if self.cycle%200==0:
+            self.groupe.shot()
+        
+             
+        # Mettez à jour la position de tous les lasers ennemis
+        bullets_e_to_remove = []  # Liste temporaire pour stocker les balles à supprimer
+        for bullet in self.groupe.bullets:
+            bullet.velocity[1] = 1 
+            bullet.move()
+
+            # Vérifier les collisions entre les lasers et le joueur
+            for bullet in self.groupe.bullets:
+                if bullet.rect.colliderect(self.ship.rect):
+                    self.groupe.bullets.remove(bullet)
+                    bullets_e_to_remove.append(bullet)  # Ajoutez la balle à la liste temporaire
+                    self.game=False
+
+        # Supprimez les balles de la liste originale
+        for bullet in bullets_e_to_remove:
+            if bullet in self.groupe.bullets:  # Vérifiez si la balle est toujours dans la liste
+                self.groupe.bullets.remove(bullet)
+                
+
+
+        # Mettre à jour la position des enemies
+        if self.cycle%3==0:
             for enemie in self.groupe.band:
-                if bullet.rect.colliderect(enemie.rect):
-                    print("i")
-                    self.groupe.band.remove(enemie)
-                    self.ship.bullets.remove(bullet)
+                
+                infoObject = pygame.display.Info()
+                w = infoObject.current_w  # Cette variable me permet de faire faire des tours d'écrans aux enemis
 
-        
-        #Mettre à jour la position des enemies
-        for enemie in self.groupe.band:
-            
-            infoObject = pygame.display.Info()
-            w=infoObject.current_w #Cette variable me permet de faire faire des toures d'écrans aux enemis
-            
-            
-          
-            if self.groupe.touch>=len(self.groupe.band)*2: #Quand chaque mob à touché le bord deux fois, je les fais descendre
-                self.anc=enemie.velocity[0] #Je mémorise leurs vélocité
-                for enemie in self.groupe.band:
-                    enemie.velocity[0] = 0
-                    enemie.velocity[1] = (w/90)*enemie.speed #Mon ennemie descend de 10% de l'ecrans
-                    self.groupe.touch=0
+                # Faire descendre les ennemis lorsqu'ils atteignent le bord de l'écran
+                if enemie.rect.left < 0 or enemie.rect.right > w:
+                    enemie.velocity[0] = -enemie.velocity[0]  # Supprimer la multiplication par enemie.speed
+                    enemie.velocity[1] = 20  # Faire descendre les ennemis
 
-            if enemie.rect.left < 0 or enemie.rect.right >w : #Quand un mob touche un bors il change de diréctions et mémorise qu'il à touché le bord
-                enemie.velocity[0] = -enemie.velocity[0]*enemie.speed
-                self.anc=enemie.velocity[0]
-                self.groupe.touch+=1
+                enemie.move()  # Je fais bouger le mob
+                enemie.velocity[1] = 0  # Réinitialiser la vitesse verticale après le mouvement
 
 
-            enemie.move() #Je fais bouger le mob
-            if enemie.velocity[0]==0: #Si les mobs sont descendu, je repasse leurs velocité à leurs valeurs avant les descentes
-                enemie.velocity[0]= self.anc
-            enemie.velocity[1]=0
-
-            
-        
+                    
+                
         # Supprimez les lasers qui ont quitté l'écran
         self.ship.bullets = [bullet for bullet in self.ship.bullets if bullet.rect.y > -bullet.sprite.get_height()]
 
         #Maintenir le vaisseau en bas de l'écran
         infoObject = pygame.display.Info()
         self.ship.rect.y= (infoObject.current_h - (infoObject.current_h)/20)-65
+
+        #Changer la vitesse des ennemis, toute les 30 seconde
+
+        self.tog=time.time() #J'enregiste le temps du jeu
+        
+        x=3 #Ici x represente la limite de vitesse
+
+        if self.score.score>10000: #Si le joueur depasse le score de 10.000 point
+            x=4 #Je change la limite de vitesse
+            self.groupe.speed =x
+            for enemie in self.groupe.band:  #Et je l'applique à ton mon groupe
+                    enemie.speed=self.groupe.speed
+
+        if self.tog - self.anctog >=10 : #Si la difference entre le moment actuel et le dernier moment de modification est plus que 10 sec, je modifie la vitesse 
+            
+            if self.groupe.speed>=x:#Si la limite est depasser, je remet ma vitesse à sa limite
+                self.groupe.speed=x
+            else:
+                self.groupe.speed=self.groupe.speed+1 #Sinon je rajoute la moitié de la vitesse actuel
+                if self.groupe.speed>=x: #Si la limite est depasser, je remet ma vitesse à sa limite
+                    self.groupe.speed=x
+
+            for enemie in self.groupe.band:  #Et je l'applique à ton mon groupe
+                    enemie.speed=self.groupe.speed
+            
+            self.anctog=self.tog #Je change la valeur du dernier moment de modification
+        
+        
+
 
         
         
@@ -190,6 +304,12 @@ class App:
         for bullet in self.ship.bullets:  # Dessinez tous les lasers
             bullet.draw()
 
+        for bullet in self.groupe.bullets:
+            bullet.draw()
+
+        
+
+        self.score.draw()
 
         pygame.display.flip()  # J'affiche tous les sprites
 
@@ -202,16 +322,17 @@ pygame.display.set_caption('Invaders') #Je choisi le nom de ma fenetre
 
 background = pygame.Surface(screen.get_size()) #Je crée mon fond d'écran 
 background = background.convert()
-#background.fill((250, 250, 250)) #Je nettoie l'écran   
+  
     
-appli=App(1) #Je définie mon application, avec une valeur de vitesse
+appli=App() #Je définie mon application, avec une valeur de vitesse
 
 
 key_events = []  # Liste pour stocker les événements clavier
 
-game=True
 
-while game==True:  # Boucle principale du jeu
+
+
+while appli.game==True:  # Boucle principale du jeu
 
     for event in pygame.event.get():  # Récupère tous les événements pygame
         if event.type == pygame.QUIT:
@@ -225,11 +346,14 @@ while game==True:  # Boucle principale du jeu
     appli.update(key_events)
     appli.draw()
 
+
     for enemis in appli.groupe.band: #Si un enemies dépasse mon vaisseau, on arrete le jeu, le joueur à perdu
         if enemis.rect.y>= appli.ship.rect.y:
-            game=False
-    
+            appli.game=False
 
+
+    
+   
     
     
     key_events.clear()  # Nettoyez la liste des événements clavier après les avoir traités
